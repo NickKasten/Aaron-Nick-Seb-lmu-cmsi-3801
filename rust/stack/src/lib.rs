1bbf33ebda3c4c5ff65241867c9f3bb005e56dbc
Nicholas <nickkasten03@gmail.com>
2024-11-17 22:48:13 -0800

    New Rustacean's Stack implementation

--- rust/stack/src/lib.rs
+++ rust/stack/src/lib.rs
@@ -5,16 +5,37 @@ pub struct Stack<T> {
 
 impl<T> Stack<T> {
     // Implement new
-
+    fn new() -> Stack<T> {
+        Stack {items: Vec::<T>::new()}
+    }
     // Implement push
-
+    fn push(&mut self, value: T) {
+        self.items.push(value);
+    }
     // Implement pop
-
+    fn pop(&mut self) -> Option<T> {
+        let value: Option<T> = self.items.pop();
+        match value {
+            Some(value) => Some(value),
+            None => None,
+        }
+    }
     // Implement peek
-
+    fn peek(&self) -> Option<&T> {
+        let value: Option<&T> = self.items.last();
+        match value {
+            Some(value) => Some(value),
+            None => None,
+        }
+    }
     // Implement is_empty
-
+    fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
     // Implement len
+    fn len(&self) -> usize {
+        self.items.len()
+    }
 }
 
 #[cfg(test)]
