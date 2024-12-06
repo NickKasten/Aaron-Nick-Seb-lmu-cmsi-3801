68b22ba7bfabbb6f216e132836d70a50f1119f56
Teuthida <33849310+Teuthida255@users.noreply.github.com>
2024-12-06 13:11:41 -0700

    Minor improvements and text fixes

--- go/restaurant.go
+++ go/restaurant.go
@@ -43,7 +43,7 @@ const eatTime = 2
 const leaveTime = 5
 func dine(name string, waiter chan *order,  wg *sync.WaitGroup) {
 	defer wg.Done()
-	log.Println(name, "has entered the restaurant.")
+	log.Println(name, "entered the restaurant.")
 	mealsEaten := 0
 	for mealsEaten < maxMeals {
 		order := newOrder(name)
@@ -55,13 +55,13 @@ func dine(name string, waiter chan *order,  wg *sync.WaitGroup) {
 			do(eatTime, name, "eating cooked order #", recievedOrder.id, "prepared by", recievedOrder.preparedBy)
 			mealsEaten++
 		case <- time.After(maxWaitTime * time.Second):
-			do(leaveTime, name, "has stormed out of the restaurant after waiting for order #", order.id)
+			do(leaveTime, name, "stormed out of the restaurant after waiting for order #", order.id)
 		}
 	}
-	log.Println(name, "is satiated and has left the restaurant.")
+	log.Println(name, "was satiated and left the restaurant.")
 }
 
-
+const cookTime = 10
 func cook(name string, waiter chan *order){
 	log.Println(name, "starting work") 
 	for {
@@ -75,7 +75,7 @@ func cook(name string, waiter chan *order){
 		//	break
 		//}
 		order := <- waiter
-		do(10, name, "cooking order", order.id, "for", order.customer)
+		do(cookTime, name, "began cooking order #", order.id, "for", order.customer)
 		order.preparedBy = name
 		order.reply <- order
 	}
@@ -87,12 +87,12 @@ func main() {
 	customers := [...]string{"Ani", "Bai", "Cat", "Dao", "Eve", "Fay", "Gus", "Hua", "Iza", "Jai"}
 	chefs := [...]string{"Remy", "Colette", "Linguini"}
 
+	for _, chef := range chefs {
+		go cook(chef, waiter)
+	}
 	for _, customer := range customers {
 		wg.Add(1)
 		go dine(customer, waiter, &wg)
-	}
-	for _, chef := range chefs {
-		go cook(chef, waiter)
 	}
 	
 	wg.Wait()
